8e73abdbc9aac67ff548cde1e00d13b8d506b494
czelig <chen.zelig@intel.com>
2015-01-07 12:02:36 +0200

    asd

--- Import Redesign/PROD/AutosysGeoAnalysis.bat
+++
@@ -1,2 +0,0 @@
-SET MGID=%1
-SecureRunAs "Software\Intel Corporation\MFGSolutions\iBIUser" "D:\Jobs\AutosysSP.bat \"USP_GM_MainProcedure @ModelGroupID=%MGID%\" MFGPROD"
--- Vmin BDW/3-Modeling/set.connection funcion.R
+++
@@ -1,7 +0,0 @@
-set.connection<-function(){
-  serverName<- "haisqldev021"
-  userName<-""
-  password<-""
-  
-  return(odbcConnect(serverName,uid=userName,pwd=password));
-}
